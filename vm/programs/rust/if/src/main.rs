b6025ecacc09692133d53db315c3218ed6f1a324
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2025-12-12 17:02:16 -0300

    Add add and if rust tests

---
+++ vm/programs/rust/if/src/main.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[unsafe(export_name = "main")]
+pub fn main() -> i32 {
+    if 1 < 2 {
+        return 10;
+    } else {
+        return 20;
+    }
+}
